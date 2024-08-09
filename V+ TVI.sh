770962e4028b0e0eb9c71003f30a8ffa473273d7
RodriPTV <161733839+RodriPTV@users.noreply.github.com>
2024-08-09 21:19:19 +0100

    Update V+ TVI.sh

--- V+ TVI.sh
+++ V+ TVI.sh
@@ -4,4 +4,4 @@ cd M3U
 
 # V+ TVI - update the stream URL of V+ TVI
 
-sed -i "/live_vmais\/live_vmais/ c https://video-auth7.iol.pt/live_vmais/live_vmais/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" V+_TVI.m3u8
+sed -i "/live_vmais\/live_vmais/ c https://video-auth7.iol.pt/live_vmais/live_vmais/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" V+_TVI.m3u
