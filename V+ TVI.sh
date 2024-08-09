18eb9c25df1d197607545e8252cfba8b916dd8ba
RodriPTV <161733839+RodriPTV@users.noreply.github.com>
2024-08-09 21:16:33 +0100

    Create V+ TVI.sh

---
+++ V+ TVI.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd M3U
+
+# V+ TVI - update the stream URL of V+ TVI
+
+sed -i "/live_vmais\/live_vmais/ c https://video-auth6.iol.pt/live_vmais/live_vmais/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" V+_TVI.m3u8
