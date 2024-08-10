4fd630fd1847778e58707775ad152d2a79580bf3
RodriPTV <161733839+RodriPTV@users.noreply.github.com>
2024-08-10 18:02:17 +0100

    Delete TVI/V+ directory

--- TVI/V+/V+ TVI.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd M3U
-
-# V+ TVI - update the stream URL of V+ TVI
-
-sed -i "/live_vmais\/live_vmais/ c https://video-auth7.iol.pt/live_vmais/live_vmais/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" V+_TVI.m3u
